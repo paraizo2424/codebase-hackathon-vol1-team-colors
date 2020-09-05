1eae680da22e6fa9d79ff32015f3beb12b664fef
paraizo2424 <e095745@ie.u-ryukyu.ac.jp>
2020-09-05 17:53:37 +0900

    微調整

--- frontend/pages/edit.vue
+++ frontend/pages/edit.vue
@@ -125,7 +125,7 @@
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
-            新規登録
+            更新
           </button>
         </div>
       </form>
