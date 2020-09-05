92208fa393338133360bdedf6717b2a5c9918236
paraizo2424 <e095745@ie.u-ryukyu.ac.jp>
2020-09-05 17:49:15 +0900

    自己紹介用のtaxtareaを追加

--- frontend/pages/edit.vue
+++ frontend/pages/edit.vue
@@ -105,6 +105,21 @@
             placeholder="Password_confirm"
           />
         </div>
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="password_confirm"
+          >
+            自己紹介
+          </label>
+          <textarea
+            id="introduce"
+            name="introduce"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            rows="5"
+            placeholder="Introduce"
+          />
+        </div>
         <div class="text-center">
           <button
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
