70c49a8dfbb501a16d74a25fe2f154264c544cfe
paraizo2424 <e095745@ie.u-ryukyu.ac.jp>
2020-09-22 00:15:14 +0900

    マス目をcanvasタグで作成した。マス目を描画しただけで、クリックした時のアクションは記載なし。

--- frontend/pages/paint.vue
+++ frontend/pages/paint.vue
@@ -2,16 +2,17 @@
   <div class="container mx-auto">
     <div class="flex mb-4">
       <div class="w-3/5">
-        <canvas
-          id="myCanvas"
-          :class="{ eraser: canvasMode === 'eraser' }"
-          width="640px"
-          height="800px"
-          @mousedown="dragStart"
-          @mouseup="dragEnd"
-          @mouseout="dragEnd"
-          @mousemove="draw"
-        ></canvas>
+        <div class="border-solid border-4">
+          <canvas
+            id="myCanvas"
+            width="800px"
+            height="2000px"
+            @mousedown="draw"
+          ></canvas>
+        </div>
+        <div class="text-center">
+          <p>&lt;&lt; 1 2 3 4 5</p>
+        </div>
       </div>
       <div class="w-2/5">
         <div class="text-center mb-5 relative">
@@ -159,11 +160,6 @@
             </div>
           </div>
         </div>
-        <!-- <button id="pen-black-button" @click="penBlack">ペン（黒）</button>
-        <button id="pen-red-button" @click="penRed">ペン（赤）</button>
-        <button id="pen-blue-button" @click="penBlue">ペン（青）</button>
-        <button id="eraser-button" @click="erase">消しゴム</button>
-        <button id="clear-button" @click="clear">クリア</button> -->
       </div>
     </div>
   </div>
@@ -184,86 +180,31 @@ export default {
     this.context = this.canvas.getContext('2d')
     this.context.lineCap = 'round'
     this.context.lineJoin = 'round'
-    this.context.lineWidth = 5
+    this.context.lineWidth = 3
     this.context.strokeStyle = '#000000'
+
+    const initialPosition = 25
+    const squaerSize = 50
+    const space = 25
+
+    for (let x = 0; x < 6; x++) {
+      for (let y = 0; y < 24; y++) {
+        this.context.strokeRect(
+          initialPosition + x * squaerSize + x * space,
+          initialPosition + y * squaerSize + y * space,
+          squaerSize,
+          squaerSize
+        )
+      }
+    }
   },
   methods: {
     // 描画
     draw(e) {
-      const x = e.layerX
-      const y = e.layerY
-
-      if (!this.isDrag) {
-        return
-      }
-
-      this.context.lineTo(x, y)
-      this.context.stroke()
+      // クリックした場所の座標
+      // const x = e.layerX
+      // const y = e.layerY
     },
-    // 描画開始（mousedown）
-    dragStart(e) {
-      const x = e.layerX
-      const y = e.layerY
-
-      this.context.beginPath()
-      this.context.lineTo(x, y)
-      this.context.stroke()
-
-      this.isDrag = true
-    },
-    // 描画終了（mouseup, mouseout）
-    dragEnd() {
-      this.context.closePath()
-      this.isDrag = false
-    },
-    // クリア
-    clear() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    },
-    // ペンモード（黒）
-    penBlack() {
-      // カーソル変更
-      this.canvasMode = 'penBlack'
-
-      // 描画設定
-      this.context.lineCap = 'round'
-      this.context.lineJoin = 'round'
-      this.context.lineWidth = 5
-      this.context.strokeStyle = '#000000'
-    },
-    // // ペンモード（赤）
-    // penRed() {
-    //   // カーソル変更
-    //   this.canvasMode = 'penRed'
-
-    //   // 描画設定
-    //   this.context.lineCap = 'round'
-    //   this.context.lineJoin = 'round'
-    //   this.context.lineWidth = 5
-    //   this.context.strokeStyle = '#FF0000'
-    // },
-    // // ペンモード（青）
-    // penBlue() {
-    //   // カーソル変更
-    //   this.canvasMode = 'penBlue'
-
-    //   // 描画設定
-    //   this.context.lineCap = 'round'
-    //   this.context.lineJoin = 'round'
-    //   this.context.lineWidth = 5
-    //   this.context.strokeStyle = '#0000FF'
-    // },
-    // // 消しゴムモード
-    // eraser() {
-    //   // カーソル変更
-    //   this.canvasMode = 'eraser'
-
-    //   // 描画設定
-    //   this.context.lineCap = 'square'
-    //   this.context.lineJoin = 'square'
-    //   this.context.lineWidth = 5
-    //   this.context.strokeStyle = '#FFFFFF'
-    // },
   },
 }
 </script>
