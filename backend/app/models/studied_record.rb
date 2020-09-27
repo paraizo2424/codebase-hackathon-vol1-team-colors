0e484cc2b5570afd355247d916f93543d9ab3275
Daichi Miyagi <96.yalpx5.ew@gmail.com>
2020-09-27 17:58:58 +0900

    モデルのアソシエーションを作成しました

--- backend/app/models/studied_record.rb
+++ backend/app/models/studied_record.rb
@@ -1,2 +1,5 @@
 class StudiedRecord < ApplicationRecord
+    belongs_to :user
+    has_many :square_record_subject
+    has_many :sunjects, through: :square_record_subject
 end
--- backend/app/models/subject.rb
+++ backend/app/models/subject.rb
@@ -1,2 +1,4 @@
 class Subject < ApplicationRecord
+    has_many :square_record_subjects
+    has_many :studied_records, through: :square_record_subjects
 end
