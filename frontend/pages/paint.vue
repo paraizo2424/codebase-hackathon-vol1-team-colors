c69510e96de41cff932acdd549132c2a06de006b
paraizo2424 <e095745@ie.u-ryukyu.ac.jp>
2020-10-08 00:21:48 +0900

    描画モードを切り替えることで色塗りと色消しができる

--- frontend/pages/paint.vue
+++ frontend/pages/paint.vue
@@ -49,34 +49,52 @@
         <h3>描画モード</h3>
         <div class="flex">
           <div class="flex-1 text-center border-solid border-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-              />
-            </svg>
+            <input
+              id="paint-mode"
+              v-model="paintMode"
+              type="radio"
+              name="selected-mode"
+              value="paint"
+            />
+            <label for="paint-mode">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                />
+              </svg>
+            </label>
           </div>
           <div class="flex-1 text-center border-solid border-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
+            <input
+              id="eraser-mode"
+              v-model="paintMode"
+              type="radio"
+              name="selected-mode"
+              value="eraser"
+            />
+            <label for="eraser-mode">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </label>
           </div>
         </div>
         <div class="border-solid border-4">
@@ -183,6 +201,7 @@
 export default {
   data() {
     return {
+      paintMode: 'paint',
       checkedSubject: '',
       records: [
         {
@@ -370,10 +389,10 @@ export default {
   },
   methods: {
     paint(record) {
-      if (record.color) {
-        record.color = ''
-      } else {
+      if (this.paintMode === 'paint') {
         record.color = this.checkedSubject
+      } else if (this.paintMode === 'eraser') {
+        record.color = ''
       }
     },
   },
